e0a245c5535a5b697b11f755ddedb48525b59733
diekmann <diekmann@net.in.tum.de>
2015-06-24 09:30:47 +0200

    implementation of JVM7 according to spec

---
+++ main.c
@@ -0,0 +1,8 @@
+int main(int argc, char** argv){
+  /*See "The Java® Virtual Machine Specification"
+    Java SE 7 Edition
+    Chapter 6.3. Virtual Machine Errors
+    https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-6.html#jvms-6.3*/
+  puts("OutOfMemoryError");
+  return(-1);
+}
